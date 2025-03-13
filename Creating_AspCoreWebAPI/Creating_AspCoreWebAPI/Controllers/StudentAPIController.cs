5fc3a11bd866c96a6e826c8cdddccd1feeb54c40
Furqan-Ur-Rehman <furqanrehman653@gmail.com>
2025-03-13 15:58:18 +0500

    Upload Restful Asp.Net Core Web API

---
+++ Creating_AspCoreWebAPI/Creating_AspCoreWebAPI/Controllers/StudentAPIController.cs
@@ -0,0 +1,72 @@
+﻿using Creating_AspCoreWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Creating_AspCoreWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentAPIController : ControllerBase
+    {
+        //Create DbContext Class Object below
+        private readonly AspCoreWebApidbContext _context;
+
+        //Create Parameterized Constructor
+        public StudentAPIController(AspCoreWebApidbContext context) 
+        {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Student>>> GetStudents() 
+        {
+            var data = await _context.Students.ToListAsync();
+            return Ok(data);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Student>> GetStudentById(int id)
+        {
+            var data_by_id = await _context.Students.FindAsync(id);
+            if (data_by_id == null)
+            { 
+                return NotFound();
+            }
+            return Ok(data_by_id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Student>> CreateStudent(Student student)
+        {
+            await _context.Students.AddAsync(student);
+            await _context.SaveChangesAsync();
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Student>> UpdateStudent(int id, Student student)
+        {
+           if(id != student.Id)
+            {
+                return BadRequest();
+            }
+           _context.Entry(student).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return Ok(student);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Student>> DeleteStudent(int id)
+        {
+            var data = await _context.Students.FindAsync(id);
+            if (data == null)
+            {
+                return NotFound();
+            }
+            _context.Students.Remove(data);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
---
+++ Creating_AspCoreWebAPI/Creating_AspCoreWebAPI/Models/AspCoreWebApidbContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Creating_AspCoreWebAPI.Models;
+
+public partial class AspCoreWebApidbContext : DbContext
+{
+    public AspCoreWebApidbContext()
+    {
+
+    }
+
+    public AspCoreWebApidbContext(DbContextOptions<AspCoreWebApidbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+    }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.FatherName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Standard)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+            entity.Property(e => e.StudentGender)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.StudentName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
